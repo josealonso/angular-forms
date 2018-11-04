ffb05d3819115eda0b4c4de1d82e657c236918ef
José Ramón Alonso Tapia <jose.alonso.programmer@gmail.com>
2018-11-04 20:47:30 +0100

    Added validations with the FormControl set as an instance variable.

--- src/app/demo-form-sku/demo-form-sku.component.ts
+++ src/app/demo-form-sku/demo-form-sku.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
 	selector: 'app-demo-form-sku',
 	templateUrl: './demo-form-sku.component.html',
 	styleUrls: [ './demo-form-sku.component.css' ]
 })
-export class DemoFormSkuComponent implements OnInit {
+export class DemoFormSkuComponent {
 	myForm: FormGroup;
+	sku: AbstractControl;
 
 	constructor(fb: FormBuilder) {
 		this.myForm = fb.group({
-			sku: [ 'ABC123' ]
+			sku: [ '', Validators.required ]
 		});
-	}
 
-	ngOnInit() {}
+		this.sku = this.myForm.controls['sku'];
+	}
 
 	onsubmit(value: string): void {
 		console.log('you submitted value:', value);
